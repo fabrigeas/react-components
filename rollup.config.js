6e972f482d9a748c9cbd25c1f9aa22f7526feb17
fabrigeas <fabrigeas@gmail.com>
2023-04-03 15:03:16 +0200

    build: configure rollup for building

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import packageJson from './package.json' assert { type: 'json' };
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import sass from 'rollup-plugin-sass';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      nodeResolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      terser(),
+      sass({ insert: true }),
+    ],
+  },
+];
