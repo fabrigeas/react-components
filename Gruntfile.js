34b1a0e1b6af2c29c22091c72db190cb7a627f59
fabrigeas <fabrigeas@gmail.com>
2023-04-15 13:03:36 +0200

    chore(grunt): add Gruntfile to automate deployment

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+module.exports = grunt => {
+  const pkg = grunt.file.readJSON('package.json');
+
+  /**
+   *
+   * @param {string} stderr
+   */
+  const revertAndAbort = error => {
+    grunt.log.error(error.toString());
+    grunt.task.run('shell:revert');
+  };
+
+  grunt.initConfig({
+    pkg,
+    shell: {
+      build: {
+        command: [
+          'npm run build',
+          'git add dist',
+          'git commit -m "update components build"',
+        ].join('&'),
+      },
+      bumpVersion: {
+        command: v => `npm version --allow-same-version ${v} -m "version bump %s"`,
+        options: {
+          stdout: false,
+          callback(error, {}, {}, callback) {
+            if (error) {
+              return revertAndAbort(error);
+            }
+            callback();
+          },
+        },
+      },
+      publishNpm: 'npm publish',
+      pushFollowTags: 'git push --follow-tags',
+      updateChangelog: {
+        command: [
+          "git log --oneline | sed 's/^[a-zA-Z0-9]* //g' > CHANGELOG.md",
+          'git add CHANGELOG.md',
+          'git commit --no-verify --amend  --no-edit',
+        ].join(';'),
+        options: {
+          stdout: false,
+          callback(error, {}, {}, callback) {
+            if (error) {
+              return revertAndAbort(error);
+            }
+            callback();
+          },
+        },
+      },
+      revert: {
+        command: [].join(';'),
+        options: {
+          callback() {
+            grunt.log.error(`failed to deploy. Reverting all changes`);
+          },
+        },
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-shell');
+
+  /**
+   * Register a grunt task for publishing this npm package
+   * USAGE:
+   * grunt publish:major // increase the npm major version, then publish to npm and push to git
+   * grunt publish:minor // increase the npm minor version, then publish to npm and push to git
+   * grunt publish:patch // increase the npm patch version, then publish to npm and push to git
+   * grunt publish // equivalent to grunt publish:patch
+   * grunt publish:typo // equivalent to grunt publish:patch
+   */
+  grunt.registerTask('publish', (version = 'patch') => {
+    const { task } = grunt;
+
+    if (!['major', 'minor', 'patch'].includes(version)) {
+      version = 'patch';
+    }
+
+    task.run('shell:build');
+    task.run(`shell:bumpVersion:${version}`);
+    task.run('shell:publishNpm');
+    task.run('shell:updateChangelog');
+    task.run('shell:pushFollowTags');
+  });
+
+  grunt.registerTask('shell:default', ['npm version']);
+};
